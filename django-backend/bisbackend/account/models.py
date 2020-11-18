9057ebfbde7d511adafd3a9f6be2f71c358e44ae
Tran Minh Duc <62131944+s3855825@users.noreply.github.com>
2020-11-18 18:50:05 +0700

    add Account table

--- django-backend/bisbackend/account/models.py
+++ django-backend/bisbackend/account/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    pass
+    # TODO: add rating_count, rating_avg, rating_value
+    id = models.AutoField(primary_key=True)
+    username = models.Charfield(max_length=20, unique=True)
+    email = models.Charfield(unique=True)
+    password_hash = models.Charfield()
