76ef476a0d0bf492e8dc956723e53dc87338617d
tuann <tuannguyen1911@outlook.com>
2020-12-22 17:38:57 +0700

    add temporary screens

---
+++ FirstReactNativeProject/app/screens/NotificationScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import TestScreen from './TestScreen';
+
+function NotificationScreen() {
+    return (
+        <TestScreen>
+            <Text>Notifications</Text>
+        </TestScreen>
+    )
+}
+
+export default NotificationScreen;
---
+++ FirstReactNativeProject/app/screens/TestScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function TestScreen({ children }) {
+    return (
+        <View style={styles.container}>
+            {children}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default TestScreen;
