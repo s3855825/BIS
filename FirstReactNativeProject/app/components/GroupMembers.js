c3178e2c6287fa42c11e27417f05a3483024fb3d
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2020-12-25 17:10:46 +0700

    Create GroupMembers.js

---
+++ FirstReactNativeProject/app/components/GroupMembers.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+import PostCard from './PostCard';
+import PostSeparator from './PostSeparator';
+import MemberCard from '../components/MemberCard';
+
+const member = [
+    {
+        'id': 1,
+        'name': 'Vinh'        
+    },
+    {
+        'id': 2,
+        'name': 'Duc'        
+    },
+]
+
+function GroupList(props) {
+    return (
+        <FlatList
+            style={{ paddingHorizontal: 20 }}
+            data={member}
+            keyExtractor={member => member.id.toString()}
+            renderItem={({ item }) =>
+                <MemberCard
+                    name = {item.name}
+                />}
+        />
+    );
+}
+
+export default GroupList;
